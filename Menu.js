12d5620b2e6e68744a1ce701651b8d65bca27c23
akhilesh091 <akhileshts091@gmail.com>
2022-12-16 11:29:10 +0530

    first commit

---
+++ Menu.js
@@ -0,0 +1,24 @@
+// function slide(){
+
+// let slide=document.getElementById('menubar')
+//  slide.style.left="0px"
+// }
+function slide(){
+    let slide=document.getElementById('menubar')
+    
+    if(slide.style.left=="-400px"){
+        slide.style.left="0px"
+        
+
+    }else{
+        slide.style.left="-400px"
+    }
+
+}
+
+
+
+
+
+
+      
